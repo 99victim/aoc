4894969fd4a5419d8a5ea57ba75fe59f5c3b32bb
starry-light88 <sung0.0hyun@gmail.com>
2025-08-08 17:56:48 +0900

    아이디 중복 검사 추가

--- aoc/src/main/java/com/project/aoc/users/UserRepository.java
+++ aoc/src/main/java/com/project/aoc/users/UserRepository.java
@@ -3,6 +3,11 @@ package com.project.aoc.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+    boolean existsByUserId(String userId);
+
 }
--- aoc/src/main/java/com/project/aoc/users/UserService.java
+++ aoc/src/main/java/com/project/aoc/users/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository repository;
 
+    //회원가입
     public UserResponse register(UserRequest request) {
         Users user = Users.builder()
                 .userId(request.getUserId())
@@ -19,5 +20,12 @@ public class UserService {
 
         return UserResponse.fromEntity(savedUser);
 
+    }
+
+    //회원가입_아이디 중복 체크
+    public void checkUserId(String userId){
+        if(repository.existsByUserId(userId)){
+            throw new IllegalArgumentException("이미 사용중인 아이디입니다");
+        }
     }
 }
